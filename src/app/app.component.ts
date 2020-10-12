3e679cb53b45c545575a0e5ec8d1586b9cae7a8c
Paul Chesa <paulchesa1@gmail.com>
2020-10-12 17:11:12 +0300

    display numbers, pick values from buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+
+  keys = [1,2,3,4,5,6,7,8,9,0];
+  keyBoardVals: number;
+  valsArr: number[] = [];
+  vals;
+
+  enteredVal(num): void{
+    this.valsArr = [...this.valsArr, num]
+    this.vals=parseInt(this.valsArr.join(""));
+    console.log(this.vals);
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
